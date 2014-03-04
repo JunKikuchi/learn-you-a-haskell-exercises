9d72ceb72be60c22141855883fb1a2980294e353
Kikuchi Jun <kikuchi@bonnou.com>
2014-03-05 00:49:01 +0900

    9章の問題を解いた

--- 9-input-and-output.hs
+++ 9-input-and-output.hs
@@ -3,11 +3,18 @@
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
+
+import System.Environment
+import System.Random
+import Data.List
   
-main = undefined
+main = getArgs >>= putStr . concat . intersperse " " . echo
+    where
+        echo ("-n":xs) = xs
+        echo xs        = xs ++ ["\n"]
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
  -}
 lottery :: StdGen -> [Int]
-lottery gen = undefined
+lottery = sort . take 6 . nub . randomRs (1, 49)
