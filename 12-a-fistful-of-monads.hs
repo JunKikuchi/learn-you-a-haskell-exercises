49897c9ea47080e55caca9de96b0fc67c61d9f4c
Kikuchi Jun <kikuchi@bonnou.com>
2014-03-12 09:11:48 +0900

    12章の問題を解いた

--- 12-a-fistful-of-monads.hs
+++ 12-a-fistful-of-monads.hs
@@ -5,27 +5,34 @@
  -   Success, which takes that parameter and
  -   Fail String, which represents a failure, and a reason for that failure
  -}
---data Validation a 
+data Validation a = Success a | Fail String deriving (Show)
 
 -- Make the Validation a Monad
-
+instance Monad Validation where
+    return = Success
+    Success a >>= f = f a
+    Fail s    >>= f = Fail s
 
 {-
  - Create a function, positiveCheck, which takes a number and returns a successful Validation if it's positive, 
  - and a failed Validation with a String message if not.
  -}
---positiveCheck :: (Num a, Ord a) => a -> Validation a
---positiveCheck x = undefined
+positiveCheck :: (Num a, Ord a) => a -> Validation a
+positiveCheck x
+    | x > 0     = Success x
+    | otherwise = Fail "It is not positive."
 
 {-
  - Create a function, evenCheck, which returns a successful Validation if it's even,
  - and a failed Validation with a string message if it's odd
  -}
---evenCheck :: (Integral a)  =>  a -> Validation a
---evenCheck x = undefined
+evenCheck :: (Integral a)  =>  a -> Validation a
+evenCheck x
+    | even x    = Success x
+    | otherwise = Fail "It is not even."
 
 {-
  - Write a function which uses positiveCheck and evenCheck to make sure a number is both positive and even
  -}
---positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
---positiveAndEvenCheck x = undefined
+positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
+positiveAndEvenCheck x = return x >>= positiveCheck >>= evenCheck
